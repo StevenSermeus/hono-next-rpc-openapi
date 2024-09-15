71281a56e3ef0c9f2d20d89c2283076b557c8eba
Steven Sermeus <stevensermeus@gmail.com>
2024-09-15 14:17:49 +0200

    Add react query dev tools

--- src/providers/react-query.tsx
+++ src/providers/react-query.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 interface ReactQueryProviderProps {
   children: React.ReactNode;
@@ -10,5 +11,10 @@ interface ReactQueryProviderProps {
 
 export default function ReactQueryProvider({ children }: ReactQueryProviderProps) {
   const [queryClient] = React.useState(() => new QueryClient({}));
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools buttonPosition="bottom-left" />
+      {children}
+    </QueryClientProvider>
+  );
 }
