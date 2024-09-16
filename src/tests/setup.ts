19eafc10eb7bc665870654ebb16af036db123408
Steven Sermeus <stevensermeus@gmail.com>
2024-09-16 21:12:41 +0200

    Add more time

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -12,7 +12,7 @@ let container: StartedPostgreSqlContainer;
 
 beforeAll(async () => {
   container = await new PostgreSqlContainer('postgres:16').start();
-  throw new Error(container.getConnectionUri());
+  console.log('container', container);
   const url = container.getConnectionUri();
   process.env.DATABASE_URL_TEST = url;
   //exec with zsh shell
@@ -35,7 +35,7 @@ beforeAll(async () => {
       },
     },
   });
-}, 30000);
+}, 100000);
 
 afterAll(async () => {
   await container.stop();
