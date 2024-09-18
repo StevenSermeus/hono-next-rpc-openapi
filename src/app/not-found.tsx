c07788783c98561f087e32a127dda0a80fc6163e
Steven Sermeus <stevensermeus@gmail.com>
2024-09-18 10:25:39 +0200

    Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+      <div className="w-full space-y-6 text-center">
+        <div className="space-y-3">
+          <h1 className="text-4xl font-bold tracking-tighter antialiased transition-transform hover:scale-110 sm:text-5xl">
+            404
+          </h1>
+          <p className="text-gray-500">
+            Looks like you&apos;ve ventured into the unknown digital realm.{' '}
+            <span className="animate-ping-slow text-xl antialiased">👻</span>
+          </p>
+        </div>
+        <Link
+          href="/"
+          className="inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          prefetch={false}
+        >
+          Return to website
+        </Link>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,20 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        // 75%, 100% {
+        //   transform: scale(2);
+        //   opacity: 0;
+        // }
+        'ping-slow': {
+          from: { transform: 'scale(1)', opacity: '1' },
+          '75%': { transform: 'scale(2)', opacity: '0' },
+          '100%': { transform: 'scale(2)', opacity: '0' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'ping-slow': 'ping 3s cubic-bezier(0, 0, 0.2, 1) infinite',
       },
     },
   },
